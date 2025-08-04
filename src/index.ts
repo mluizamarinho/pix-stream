31833e084591789ef7a0ae760fc4d5a7a021f957
Maria Luiza <maria.marinho@ccc.ufcg.edu.br>
2025-08-03 22:52:28 -0300

    corrigindo caminho endpoint de POST

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import pixRoutes from './routes/pix.route';
+import utilRoutes from './routes/util.route';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,7 @@ app.use(express.json());
 console.log("Iniciando app...");
 
 app.use('/api/pix', pixRoutes);
+app.use('/api/util', utilRoutes)
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
